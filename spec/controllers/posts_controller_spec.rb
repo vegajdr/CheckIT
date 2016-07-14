d9d9994229a6f7b329570ad51bef1b22adf006cf
vegajdr <jorgevp5@gmail.com>
2016-07-14 09:15:17 -0400

    Before database migration

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -14,7 +14,22 @@ RSpec.describe PostsController, type: :controller do
     expect(user.posts.count).to eq old_count + 1
     expect(user.posts.last.title).to eq "This is my first post"
     expect(subcheckit.posts.last.title).to eq "This is my first post"
+  end
 
+  it "allows users to edit posts" do
+    user = create :user
+    subcheckit = create :subcheckit
+    sign_in user
+
+    post = user.posts.create( title: "Initial", content: "Post", subcheckit_id: subcheckit.id)
+
+    old_count = user.posts.count
+
+    response = post :update, id: post.id, post: { title: "This is my first post", content: "Just some random content" }
+
+    expect(user.posts.count).to eq old_count
+    expect(user.posts.last.title).to eq "This is my first post"
+    expect(user.posts.last.content).to eq "Just some random content"
 
   end
 
